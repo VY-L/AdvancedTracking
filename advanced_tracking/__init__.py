958bb72f6cc00a6ecc0a75ac53bbdc421fa4739e
VY-L <V__Y@outlook.com>
2025-05-28 18:39:24 -0500

    set up some config, added ScriptLoaded

--- advanced_tracking/__init__.py
+++ advanced_tracking/__init__.py
@@ -1,9 +1,19 @@
 from mcdreforged.plugin.si.plugin_server_interface import PluginServerInterface
 from mcdreforged.plugin.si.server_interface import ServerInterface
+from mcdreforged.utils.serializer import Serializable
 import os
 import shutil
 
 PLUGIN_PATH = r"plugins\AdvancedTracking"
+
+SERVER_PATH = r'.\server'
+
+class PermissionConfig(Serializable):
+    pass
+
+class Config(Serializable):
+    permissions: PermissionConfig = PermissionConfig()
+
 
 def load_scripts(server: ServerInterface, src=r'plugins\advancedTracking\Advanced_tracking\scripts', dst=r'server\world\scripts'):
     # print(os.listdir(src))
@@ -13,6 +23,24 @@ def load_scripts(server: ServerInterface, src=r'plugins\advancedTracking\Advance
 
 
 def on_load(server:PluginServerInterface, prev):
+    global config
+    config = server.load_config_simple(target_class=Config)
     # print(os.getcwd())
     load_scripts(server=server)
     print("Advanced Tracking loaded")
+    
+    
+class ScriptLoader():
+    def __init__(self, server:ServerInterface, src, dst):
+        self.src:str = src
+        self.dst:str = dst
+        self.server:ServerInterface = server
+    
+    def reload_all(self):
+        shutil.copytree(self.src, self.dst, dirs_exist_ok=True)
+        self.server.execute("script load advanced_tracking global")
+    
+    def reload_scoreboards(self):
+        shutil.copy()
+
+
