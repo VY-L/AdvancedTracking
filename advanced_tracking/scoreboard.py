2057026a761c5ab48b2b4cfbdc73eda894a882a2
VY-L <V__Y@outlook.com>
2025-07-15 11:06:47 -0700

    rewrote scoreboard and tracker with mcdr serializer

--- advanced_tracking/scoreboard.py
+++ advanced_tracking/scoreboard.py
@@ -1,45 +1,40 @@
 import json
 from typing import List, Dict, Optional, Type
+
+from mcdreforged.utils.serializer import Serializable
 from typing_extensions import Self
 from advanced_tracking.tracker import Tracker
 
 
-class TrackerScoreboardConfig:
+class TrackerScoreboardConfig(Serializable):
     '''
     records a tracker and its weight inside a scoreboard
     '''
-    def __init__(self, tracker_id:str, weight:int=1):
-        self.tracker_id = tracker_id
-        self.weight:int = weight
+    tracker_id: str
+    weight: int
+
 
     def to_script(self) -> Dict:
         '''
         used to generate the json file that the carpet script uses
         '''
         return {"tracker_id":self.tracker_id, "weight":self.weight}
-    
-    def to_dict(self) -> Dict:
-        return self.to_script()
 
-    @classmethod
-    def from_dict(cls, data:Dict) -> Self:
-        return cls(data["tracker_id"], data["weight"])
 
-class Scoreboard:
+class Scoreboard(Serializable):
     '''
     A scoreboard class
 
     should load and save into config files
 
     '''
-    def __init__(self, objective:str, display_name:Optional[str] = None, mode:str="weighted_sum", comments:Optional[str] = None):
-        if display_name is None:
-            display_name = objective
-        self.id:str = objective
-        self.display_name:str = display_name
-        self.mode:str = mode
-        self.trackers:List[TrackerScoreboardConfig] = []
-        self.comments = comments if comments is not None else ""
+
+    id: str
+    display_name: Optional[str] = None
+    mode: str = "weighted_sum"  # or "average", "max", etc., not supported yet
+    trackers: List[TrackerScoreboardConfig] = []
+    comments: str = ""
+
 
     def add_tracker(self, tracker:TrackerScoreboardConfig) -> None:
         '''
@@ -52,36 +47,19 @@ class Scoreboard:
         used to generate the json file that the carpet script uses
         '''
         # tracker_dicts = list(map(lambda tsc: tsc.to_script, self.trackers))
-        tracker_dicts = [tsc.to_dict() for tsc in self.trackers]
+        tracker_dicts = [tsc.to_script() for tsc in self.trackers]
         return {
             "display_name": self.display_name,
             "mode": self.mode,
             "trackers": tracker_dicts
         }
 
-    def to_dict(self) -> Dict:
-        return {
-            "id": self.id,
-            "display_name": self.display_name,
-            "mode": self.mode,
-            "trackers": [tracker.to_dict() for tracker in self.trackers],
-            "comments": self.comments
-        }
-
-    @classmethod
-    def from_dict(cls, data:Dict) -> Self:
-        tracker = cls(data["id"], data["display_name"], data["mode"], data.get("comments", ""))
-        for tracker_weight_pair in data["trackers"]:
-            tracker_scoreboard_config = TrackerScoreboardConfig.from_dict(tracker_weight_pair)
-            tracker.trackers.append(tracker_scoreboard_config)
-        return tracker
 
 
-
-
-class ScoreboardRegistry:
-    def __init__(self):
-        self.scoreboards: List[Scoreboard] = []
+class ScoreboardRegistry(Serializable):
+    scoreboards: List[Scoreboard] = []
+    # def __init__(self):
+    #     self.scoreboards: List[Scoreboard] = []
 
     def add(self, scoreboard: Scoreboard) -> None:
         self.scoreboards.append(scoreboard)
@@ -98,23 +76,6 @@ class ScoreboardRegistry:
         """
         self.scoreboards = [sb for sb in self.scoreboards if sb.id != scoreboard_id]
 
-    # def all(self) -> List[Scoreboard]:
-    #     return list(self.scoreboards.values())
-
-
-    def to_dict(self) -> Dict:
-        return {
-            "scoreboards": [sb.to_dict() for sb in self.scoreboards]
-        }
-
-    @classmethod
-    def from_dict(cls, data: Dict) -> Self:
-        instance = cls()
-        instance.scoreboards.clear()
-        for sb_data in data.get("scoreboards", []):
-            sb = Scoreboard.from_dict(sb_data)
-            instance.add(sb)
-        return instance
 
     def to_script(self) -> Dict:
         return {sb.id: sb.to_script() for sb in self.scoreboards}
@@ -122,11 +83,11 @@ class ScoreboardRegistry:
 if __name__ == "__main__":
     # Example usage
     scoreboard_registry = ScoreboardRegistry()
-    scoreboard = Scoreboard("example_scoreboard", "Example Scoreboard")
-    scoreboard.add_tracker(TrackerScoreboardConfig("tracker1", 2))
+    scoreboard = Scoreboard(id = "example_scoreboard", display_name = "Example Scoreboard")
+    scoreboard.add_tracker(TrackerScoreboardConfig(tracker_id = "tracker1", weight = 2))
     scoreboard_registry.add(scoreboard)
 
-    print(json.dumps(scoreboard_registry.to_dict(), indent=4, sort_keys=True))
-    loaded_registry = ScoreboardRegistry.from_dict(scoreboard_registry.to_dict())
+    print(json.dumps(scoreboard_registry.serialize(), indent=4, sort_keys=True))
+    loaded_registry = ScoreboardRegistry.deserialize(scoreboard_registry.serialize())
     print(json.dumps(loaded_registry.to_script(), indent=4, sort_keys=True))
     # print(loaded_registry.all())
--- advanced_tracking/tracker.py
+++ advanced_tracking/tracker.py
@@ -1,21 +1,16 @@
 import json
 from typing import Dict, Optional, List, Literal
+
+from mcdreforged.utils.serializer import Serializable
 from typing_extensions import Self
 
 TrackerType = Literal["player_break_blocks", "player_place_blocks"]
 
-class TrackerComponent:
-    def __init__(self, id: str, area: Dict[str, int], block_type: Dict, comments: Optional[str] = None):
-        self.id = id
-        self.area = area
-        self.block_type = block_type
-        self.comments = comments if comments is not None else ""
-
-    # def update(self, area: Optional[Dict[str, int]] = None, block_type: Optional[Dict] = None):
-    #     if area is not None:
-    #         self.area = area
-    #     if block_type is not None:
-    #         self.block_type = block_type
+class TrackerComponent(Serializable):
+    id: str
+    area: Dict[str, int] = {}
+    block_type: Dict[str, str] = {}
+    comments: str = ""
 
     def to_script(self) -> Dict:
         return {
@@ -23,60 +18,16 @@ class TrackerComponent:
             "block_type": self.block_type
         }
 
-    @classmethod
-    def from_dict(cls, data: dict) -> Self:
-        return cls(
-            id=data["id"],
-            area=data["area"],
-            block_type=data["block_type"],
-            comments=data.get("comments", "")
-        )
 
-    def to_dict(self) -> Dict:
-        return {
-            "id": self.id,
-            "area": self.area,
-            "block_type": self.block_type,
-            "comments": self.comments
-        }
-
-class Tracker:
-    def __init__(self, tracker_id: str, tracker_type: TrackerType, area: Optional[Dict[str, int]] = None, comments: Optional[str] = None):
-        # if tracker_type not in ("player_break_blocks", "player_place_blocks"):
-        #     raise ValueError("tracker_type must be 'player_break_blocks' or 'player_place_blocks'")
-        self.id = tracker_id
-        self.type = tracker_type
-        self.area = area if area is not None else {}
-        self.components: List[TrackerComponent] = []
-        self.comments:str = comments if comments is not None else ""
-
-    def add_component(self, name: str, component: TrackerComponent) -> None:
+class Tracker(Serializable):
+    id: str
+    type: TrackerType
+    # type: str
+    area: Dict[str, int]={}
+    components: List[TrackerComponent] = []
+    comments: str = ""
+    def add_component(self, component: TrackerComponent) -> None:
         self.components.append(component)
-
-    def to_dict(self) -> Dict:
-        return {
-            "id": self.id,
-            "type": self.type,
-            "area": self.area,
-            "components": [comp.to_dict() for comp in self.components],
-            "comments": self.comments
-        }
-
-    @classmethod
-    def from_dict(cls, data: dict) -> Self:
-        tracker = cls(
-            tracker_id=data["id"],
-            tracker_type=data["type"],
-            area=data.get("area", {}),
-            comments=data.get("comments", "")
-        )
-        # 兼容组件为列表或字典
-        components = data.get("components", [])
-        if isinstance(components, dict):
-            components = list(components.values())
-        for comp_data in components:
-            tracker.components.append(TrackerComponent.from_dict(comp_data))
-        return tracker
 
     def to_script(self) -> Dict:
         return {
@@ -84,23 +35,13 @@ class Tracker:
             "components": {comp.id: comp.to_script() for comp in self.components}
         }
 
-class TrackerRegistry:
-    def __init__(self):
-        self.trackers: List[Tracker] = []
+class TrackerRegistry(Serializable):
+    """Registry for all trackers."""
+    trackers: List[Tracker]=[]
 
     def add(self, tracker: Tracker) -> None:
         self.trackers.append(tracker)
 
-    def to_dict(self) -> Dict[str, Dict]:
-        return {tracker.id: tracker.to_dict() for tracker in self.trackers}
-
-    @classmethod
-    def from_dict(cls, data: dict) -> Self:
-        registry = cls()
-        for tracker_id, tracker_data in data.items():
-            tracker = Tracker.from_dict(tracker_data)
-            registry.trackers.append(tracker)
-        return registry
 
     def update_json_file(self, file_path: str) -> None:
         # add all trackers to "default group"，area would be empty
@@ -117,18 +58,19 @@ if __name__ == "__main__":
     # Example usage
     tracker_registry = TrackerRegistry()
 
-    tracker1 = Tracker("tracker1", "player_break_blocks", area={"x": 10, "y": 20, "z": 30})
-    component1 = TrackerComponent("component1", {"x_min": 5, "y_min": 5, "z_min": 5}, {"type": "stone"})
-    tracker1.add_component("component1", component1)
+    tracker1 = Tracker(id = "tracker1", type="player_break_blocks", area={"x": 10, "y": 20, "z": 30})
+    component1 = TrackerComponent(id = "component1", area={"x_min": 5, "y_min": 5, "z_min": 5}, block_type = {"type": "stone"})
+    TrackerComponent.deserialize(component1.serialize())
+    # tracker1.add_component("component1")
 
     tracker_registry.add(tracker1)
 
-    data = tracker_registry.to_dict()
+    data = tracker_registry.serialize()
     print(json.dumps(data, indent=4, sort_keys=True))
 
-    reloaded_tr = TrackerRegistry.from_dict(data)
+    reloaded_tr = TrackerRegistry.deserialize(data)
 
-    print(data.__eq__(reloaded_tr.to_dict()))
+    print(data.__eq__(reloaded_tr.serialize()))
 
 
 
